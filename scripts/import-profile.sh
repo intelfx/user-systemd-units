b2a274bcb129510277b341eb5cac1a604140670c
Ivan Shapovalov <intelfx@intelfx.name>
2020-02-28 08:22:22 +0300

    scripts: import-profile.sh: unset $DBUS_SESSION_BUS_ADDRESS before sourcing scripts
    $DBUS_SESSION_BUS_ADDRESS is set by logind when the user session is
    spawned (including the session that runs `systemd --user` itself).
    However, dbus-daemon (dbus-service) only runs After=basic.target,
    whereas import-profile.service runs Before=basic.target (which is
    the whole point of it).
    
    Therefore, unset $DBUS_SESSION_BUD_ADDRESS to prevent deadlocks from
    happening when a profile.d script tries to access the user bus (e. g.
    flatpak does that). This is ugly and technically incorrect, but I see
    no other viable option.

--- scripts/import-profile.sh
+++ scripts/import-profile.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+# sometimes scripts in /etc/profile.d want to access the user bus,
+# which is not yet ready by this point
+# moving import-profile.sh after basic.target creates a race (naturally,
+# we want environment to be set _before_ we get to launching user stuff)
+# and moving dbus inside basic.target does not solve anything because
+# the actual bus-activated processes will still block on basic.target.
+# No good way to solve this. Let's just disable access to dbus for now.
+export DBUS_SESSION_BUS_ADDRESS=
+
 for f in /etc/profile ~/.profile; do
 	. "$f"
 done
 
+# don't import an empty $DBUS_SESSION_BUS_ADDRESS
+unset DBUS_SESSION_BUS_ADDRESS
 systemctl --user import-environment
