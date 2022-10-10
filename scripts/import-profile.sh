64b66743ec08c6b9c369e790aa7a7a3096984395
Ivan Shapovalov <intelfx@intelfx.name>
2022-10-10 06:54:10 +0300

    scripts: import-profile.sh: rework, only import modified environment
    Sample the environment block before and after sourcing the profile
    scripts, and only import those variables that have been set or modified.

--- scripts/import-profile.sh
+++ scripts/import-profile.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+shopt -s lastpipe
+
+# Read initial environment to avoid importing variables that are set by the shell environment rather than the profile scripts.
+declare -A ENV
+env -0 | while IFS='=' read -d '' k v; do
+	ENV["$k"]="$v"
+done
 
 # sometimes scripts in /etc/profile.d want to access the user bus,
 # which is not yet ready by this point
@@ -15,12 +23,25 @@ done
 
 # don't import an empty $DBUS_SESSION_BUS_ADDRESS
 unset DBUS_SESSION_BUS_ADDRESS
+
 # in case of UID 0, override bogus $SHELL
 if (( UID == 0 )); then
 	export SHELL="$(getent passwd "$UID" | cut -d: -f7)"
-	if ! [ -n "$SHELL" -a -x "$SHELL" ] || ! grep -Fx "$SHELL" /etc/shells; then
+	if ! [[ "$SHELL" && -x "$SHELL" ]] || ! grep -q -Fx "$SHELL" /etc/shells; then
 		unset SHELL
 	fi
 fi
-# FIXME refactor, only import environment variables set by profile scripts
-systemctl --user import-environment
+
+# Read final environment and only mark changed variables for import.
+declare -A ENV_ADD
+env -0 | while IFS='=' read -d '' k v; do
+	if [[ "${ENV["$k"]}" != "$v" ]]; then
+		ENV_ADD["$k"]=1
+	fi
+done
+
+if (( ${#ENV_ADD[@]} > 0 )); then
+	echo "Importing environment modified by /etc/profile:" >&2
+	printf "* %s\n" "${!ENV_ADD[@]}"
+	systemctl --user import-environment "${!ENV_ADD[@]}"
+fi
